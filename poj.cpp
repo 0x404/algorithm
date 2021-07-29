f54ca492c87c44a50c76e59b765a64bebf67994a
0x404 <871206929@qq.com>
2021-07-29 14:07:40 +0800

    Update poj.cpp

--- poj.cpp
+++ poj.cpp
@@ -1,7 +1,7 @@
 /*
  * @author: 0x404
  * @Date: 2021-07-29 14:03:54
- * @LastEditTime: 2021-07-29 14:04:25
+ * @LastEditTime: 2021-07-29 14:07:29
  * @Description: 
  */
 #include <cstdio>
@@ -12,5 +12,6 @@ using namespace std;
 int main()
 {
     cout << "hello world" << endl;
+    cout << "this is a change" << endl;
     return 0;
 }
