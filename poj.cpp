721dead5bdd814e4999d36c06918d669f1be6b96
0x404 <871206929@qq.com>
2021-07-29 14:04:55 +0800

    Create poj.cpp

---
+++ poj.cpp
@@ -0,0 +1,16 @@
+/*
+ * @author: 0x404
+ * @Date: 2021-07-29 14:03:54
+ * @LastEditTime: 2021-07-29 14:04:25
+ * @Description: 
+ */
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    return 0;
+}
